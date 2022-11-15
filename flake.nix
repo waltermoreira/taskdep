48dfc6a3e6e22ddad989f23ba9d3148af5d0a8d7
Walter Moreira <walter@waltermoreira.net>
2022-11-14 20:33:11 -0600

    Improve caching for rust flak

--- flake.nix
+++ flake.nix
@@ -4,34 +4,35 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
+    crane.url = "github:ipetkov/crane";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, crane }:
 
     flake-utils.lib.eachSystem [
       flake-utils.lib.system.x86_64-linux
       flake-utils.lib.system.x86_64-darwin
     ]
       (system:
-        let pkgs = nixpkgs.legacyPackages.${system};
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          craneLib = crane.lib.${system};
         in
         rec {
           packages.taskdep =
-            pkgs.stdenv.mkDerivation {
-              name = "taskdep";
-              src = self;
-              buildPhase = ''
-                cargo build
-              '';
-              installPhase = ''
-                mkdir -p $out/bin; cp target/debug/taskdep $out/bin
-              '';
-              buildInputs = [ pkgs.rustc pkgs.cargo ];
-            }
-          ;
+            craneLib.buildPackage {
+              src = craneLib.cleanCargoSource ./.;
+              cargoTestCommand = "";
+              buildInputs = with pkgs; [
+                graphviz
+                libiconv
+                darwin.apple_sdk.frameworks.Security
+              ];
+            };
           packages.default = packages.taskdep;
-          devShells.default = pkgs.mkShell { 
-            buildInputs = [ pkgs.cargo pkgs.rustc pkgs.go-task ]; };
+          devShells.default = pkgs.mkShell {
+            buildInputs = [ pkgs.cargo pkgs.rustc pkgs.go-task ];
+          };
         });
 
 }
