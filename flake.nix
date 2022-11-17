7f8f57d3cf04161cbc5c84857b8e8d99a1f7d2ea
Walter Moreira <walter@waltermoreira.net>
2022-11-17 15:52:18 -0600

    Use the new shell-utils flake

--- flake.nix
+++ flake.nix
@@ -1,13 +1,16 @@
 {
-  description = "A very basic flake";
+  description = "Draw dependency graphs from a Taskfile";
 
+  # Modify taskdep to get a path for `dot` that we pass at compilation time
+  # using a patch phase? or configure phase?
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
     crane.url = "github:ipetkov/crane";
+    shell-utils.url = "github:waltermoreira/shell-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, crane }:
+  outputs = { self, nixpkgs, flake-utils, crane, shell-utils }:
 
     flake-utils.lib.eachSystem [
       flake-utils.lib.system.x86_64-linux
@@ -17,21 +20,30 @@
         let
           pkgs = nixpkgs.legacyPackages.${system};
           craneLib = crane.lib.${system};
+          shell = shell-utils.myShell.${system};
         in
         rec {
           packages.taskdep =
             craneLib.buildPackage {
+              configurePhase = ''
+                echo "In configure"
+              '';
               src = craneLib.cleanCargoSource ./.;
-              cargoTestCommand = "";
+              #cargoTestCommand = "";
               buildInputs = with pkgs; [
                 graphviz
                 libiconv
-                darwin.apple_sdk.frameworks.Security
               ];
             };
           packages.default = packages.taskdep;
-          devShells.default = pkgs.mkShell {
-            buildInputs = [ pkgs.cargo pkgs.rustc pkgs.go-task ];
+          devShells.default = shell {
+            packages = [
+              pkgs.graphviz
+              packages.taskdep
+              pkgs.cargo
+              pkgs.rustc
+              pkgs.go-task
+            ];
           };
         });
 
